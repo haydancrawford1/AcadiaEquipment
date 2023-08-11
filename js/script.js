2b8aedf0d3347c11ed4a2c9b6d9822defc1a71e1
haydancrawford1 <139697121+haydancrawford1@users.noreply.github.com>
2023-08-10 21:41:30 -0400

    update to site

--- js/script.js
+++ js/script.js
@@ -44,3 +44,17 @@ form.addEventListener("submit", function (e) {
       }, 5000);
     });
 });
+'use strict';
+
+(function ($) {
+    var $grid = $('.masonry-wrapper').masonry({
+        itemSelector: '.item',
+        columnWidth: '.item',
+        percentPosition: true,
+        transitionDuration: 300,
+    });
+
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry();
+    });
+}(jQuery));
